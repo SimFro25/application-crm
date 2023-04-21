9798f9340ea14d65a07f2b17769c38dce744de47
SimFro25 <126108417+SimFro25@users.noreply.github.com>
2023-04-21 19:51:58 +0200

    AT-4: Rework stations management (#4)
    * AT-4: Add postgres driver for work with database.
    
    * AT-4: Add builder in Station class
    
    * AT-4: Rework methods for work with DataBase instead of collection

--- src/main/java/com/simon/application/service/StationService.java
+++ src/main/java/com/simon/application/service/StationService.java
@@ -3,47 +3,93 @@ package com.simon.application.service;
 import com.simon.application.model.Station;
 import org.springframework.stereotype.Service;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class StationService {
 
-    List<Station> stations = new ArrayList<>();
-    Long sequence = 1L;
+    Connection connection;
+
+    public StationService() {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/railway", "postgres", "postgres");
+        } catch (SQLException e) {
+            System.out.println("Can't connection to the database");
+        }
+    }
 
     public List<Station> getAllStations() {
+        List<Station> stations = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM stations");
+
+            while (result.next()) {
+                stations.add(Station.builder()
+                        .id(result.getLong("id"))
+                        .name(result.getString("name"))
+                        .latitude(result.getDouble("latitude"))
+                        .longitude(result.getDouble("longitude"))
+                        .build());
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return stations;
     }
 
     public Station getStationById(long id) {
-        for (Station station : stations) {
-            if (station.getId() == id) {
-                return station;
-            }
-         }
+        Station station = null;
 
-        return null;
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM stations WHERE id = " + id + "");
+
+            if (result.next()) {
+               station = Station.builder()
+                        .id(result.getLong("id"))
+                        .name(result.getString("name"))
+                        .latitude(result.getDouble("latitude"))
+                        .longitude(result.getDouble("longitude"))
+                        .build();
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return station;
     }
 
     public void create(String name, double latitude, double longitude) {
-        stations.add(new Station(sequence++, name, latitude, longitude));
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("INSERT INTO stations (name, latitude, longitude) VALUES ('" + name + "', " + latitude + ", " + longitude + ")");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void update(long id, String name, double latitude, double longitude) {
-        Station station = getStationById(id);
-        int index = stations.indexOf(station);
-
-        station.setName(name);
-        station.setLongitude(longitude);
-        station.setLatitude(latitude);
-
-        stations.set(index, station);
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("UPDATE stations SET name = '" + name + "', latitude = " + latitude + ", longitude = " + longitude + "  WHERE id = " + id + "");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void remove(long id) {
-        Station station = getStationById(id);
-        stations.remove(station);
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("DELETE FROM stations WHERE id = " + id + "");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
