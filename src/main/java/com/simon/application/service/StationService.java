7bd200dcdd1b2d948f5cef798b6ae2ad9c2fa80b
SimFro25 <126108417+SimFro25@users.noreply.github.com>
2023-05-05 19:10:09 +0200

    AT-5: Create DBHelper to simplify connection between project and database (#6)

--- src/main/java/com/simon/application/service/StationService.java
+++ src/main/java/com/simon/application/service/StationService.java
@@ -1,6 +1,7 @@
 package com.simon.application.service;
 
 import com.simon.application.model.Station;
+import com.simon.application.util.DBHelper;
 import org.springframework.stereotype.Service;
 
 import java.sql.*;
@@ -10,21 +11,17 @@ import java.util.List;
 @Service
 public class StationService {
 
-    Connection connection;
+    DBHelper dbHelper;
 
     public StationService() {
-        try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/railway", "postgres", "postgres");
-        } catch (SQLException e) {
-            System.out.println("Can't connection to the database");
-        }
+        dbHelper = new DBHelper();
     }
 
     public List<Station> getAllStations() {
         List<Station> stations = new ArrayList<>();
 
         try {
-            Statement statement = connection.createStatement();
+            Statement statement = dbHelper.getConnection().createStatement();
             ResultSet result = statement.executeQuery("SELECT * FROM stations");
 
             while (result.next()) {
@@ -46,7 +43,7 @@ public class StationService {
         Station station = null;
 
         try {
-            Statement statement = connection.createStatement();
+            Statement statement = dbHelper.getConnection().createStatement();
             ResultSet result = statement.executeQuery("SELECT * FROM stations WHERE id = " + id + "");
 
             if (result.next()) {
@@ -67,10 +64,7 @@ public class StationService {
 
     public void create(String name, double latitude, double longitude) {
         try {
-           // Statement statement = connection.createStatement();
-           // statement.execute("INSERT INTO stations (name, latitude, longitude) VALUES ('" + name + "', " + latitude + ", " + longitude + ")");
-
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO stations (name, latitude, longitude) VALUES (?, ?, ?)");
+            PreparedStatement preparedStatement = dbHelper.getConnection().prepareStatement("INSERT INTO stations (name, latitude, longitude) VALUES (?, ?, ?)");
 
             preparedStatement.setString(1, name);
             preparedStatement.setDouble(2, latitude);
@@ -84,10 +78,7 @@ public class StationService {
 
     public void update(long id, String name, double latitude, double longitude) {
         try {
-            // Statement statement = connection.createStatement();
-            // statement.execute("UPDATE stations SET name = '" + name + "', latitude = " + latitude + ", longitude = " + longitude + "  WHERE id = " + id + "");
-
-            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE stations SET name = ?, latitude = ?, longitude = ?  WHERE id = ?");
+            PreparedStatement preparedStatement = dbHelper.getConnection().prepareStatement("UPDATE stations SET name = ?, latitude = ?, longitude = ?  WHERE id = ?");
 
             preparedStatement.setString(1, name);
             preparedStatement.setDouble(2, latitude);
@@ -102,10 +93,7 @@ public class StationService {
 
     public void remove(long id) {
         try {
-            // Statement statement = connection.createStatement();
-            // statement.execute("DELETE FROM stations WHERE id = " + id + "");
-
-            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM stations WHERE id = ?");
+            PreparedStatement preparedStatement = dbHelper.getConnection().prepareStatement("DELETE FROM stations WHERE id = ?");
 
             preparedStatement.setLong(1, id);
 
@@ -114,5 +102,4 @@ public class StationService {
             throw new RuntimeException(e);
         }
     }
-
 }
