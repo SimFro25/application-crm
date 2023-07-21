8824c1599de0e8c90ba7e09b8c211303603a737a
SimFro25 <126108417+SimFro25@users.noreply.github.com>
2023-07-21 18:36:08 +0200

    AT-9: Implementation route management

---
+++ src/main/java/com/simon/application/controller/RouteController.java
@@ -0,0 +1,59 @@
+package com.simon.application.controller;
+
+import com.simon.application.service.RouteService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("route")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class RouteController {
+
+    RouteService routeService;
+
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("routes", routeService.getAll());
+        return "route/list";
+    }
+
+    @GetMapping("create")
+    public String create(Model model) {
+        return "route/create";
+    }
+
+    @PostMapping("create")
+    public String create(@RequestParam("name") String name) {
+        routeService.create(name);
+        return "redirect:/route";
+    }
+
+    @GetMapping("{id}")
+    public String view(Model model, @PathVariable long id) {
+        model.addAttribute("route", routeService.getRouteById(id));
+        return "route/view";
+    }
+
+    @GetMapping("{id}/edit")
+    public String edit(Model model, @PathVariable long id) {
+        model.addAttribute("route", routeService.getRouteById(id));
+        return "route/edit";
+    }
+
+    @PostMapping("{id}/edit")
+    public String edit(@PathVariable long id, @RequestParam("name") String name) {
+        routeService.edit(id, name);
+        return "redirect:/route";
+    }
+
+    @GetMapping("{id}/delete")
+    public String delete(@PathVariable long id) {
+        routeService.delete(id);
+        return "redirect:/route";
+    }
+}
