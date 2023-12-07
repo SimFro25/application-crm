47c9de2070ae836d4575c375a26ec8563d6b61a2
SimFro25 <senafrolov2005@gmail.com>
2023-12-07 19:07:13 +0100

    AT-16: Fix entity name in ExemptionRepository.class

--- src/main/java/com/simon/application/repository/ExemptionRepository.java
+++ src/main/java/com/simon/application/repository/ExemptionRepository.java
@@ -1,9 +1,10 @@
 package com.simon.application.repository;
 
+import com.simon.application.entity.Exemption;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExemptionRepository extends CrudRepository<Exception, Long> {
+public interface ExemptionRepository extends CrudRepository<Exemption, Long> {
 
 }
