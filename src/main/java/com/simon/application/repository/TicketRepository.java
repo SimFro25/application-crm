58b8f6385b827288e22fa2db535d4a59f9ef6a19
SimFro25 <senafrolov2005@gmail.com>
2023-12-25 18:03:46 +0100

    AT-17: Create repository for Ticket

---
+++ src/main/java/com/simon/application/repository/TicketRepository.java
@@ -0,0 +1,10 @@
+package com.simon.application.repository;
+
+import com.simon.application.entity.Ticket;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends CrudRepository<Ticket, Long> {
+
+}
